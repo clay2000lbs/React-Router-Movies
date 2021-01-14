78f56475061dd5a58c001a494bc77818b0350975
unknown <clay2000lbs@gmail.com>
2021-01-13 21:26:46 -0600

    hourly commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { Route, Link, Switch} from 'react-router-dom';
 import axios from 'axios';
 import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
